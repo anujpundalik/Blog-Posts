b1479c8699f08367933c74f7a2bcee35948e3e57
Anuj Pundalik <pundalik.anuj@gmail.com>
2019-11-05 15:11:58 +0530

    Basic display done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,16 @@
 import JsonPlaceHolder from '../api/JsonPlaceHolder';
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+
+    await dispatch (fetchPosts());
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+}
 
 export const fetchPosts = () => async dispatch => {
     
@@ -10,3 +22,27 @@ export const fetchPosts = () => async dispatch => {
     });
     
 };
+
+export const fetchUser = (id) => async dispatch => {
+
+    const response = await JsonPlaceHolder.get( `/users/${id}`);
+    
+
+    dispatch ({ 
+        type : 'FETCH_USER',
+        payload : response.data
+    });
+   
+};
+
+// const _fetchUser =  _.memoize( async( id, dispatch ) => {
+
+//     const response = await JsonPlaceHolder.get(`/users/${id}`);
+
+//     dispatch ({
+        
+//         type : 'FETCH_USER',
+//         payload : response.data
+//     });
+    
+// });
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import PostList from './PostList'
+import PostList from './PostList';
 
 const App = () => {
     return (
         <div className = 'ui container'>
-            <PostList/>
+            <PostList/> 
         </div>
     );
 
